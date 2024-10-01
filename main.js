6fdff53156a0700f631bfa2dc29431d6ef263c46
memirkocak <mekocak@edenschool>
2024-10-01 09:33:01 +0200

    reaction time

--- main.js
+++ main.js
@@ -13,26 +13,41 @@ const reactionTime = document.querySelector('#reactionTime');
         
         body.innerHTML = `
             <div class="reactionTime">
-                <p id="reactionTime-time">aaa</p>
+                <p id="reactionTime-time">00:00</p>
                 <button class="reactionTime-timeBtn">WAIT GREEN...</button>
             </div>
         `
 
-        let randomTime = Math.floor(Math.random() * 10000 + 1000)
+        let randomTime = Math.floor(Math.random() * 2000 + 1000)
         console.log(randomTime);
         const btnTime = document.querySelector('.reactionTime-timeBtn')
+        const reactionTimeText = document.querySelector('#reactionTime-time')
 
+        
         const setIntervalTime = setInterval(() => {
             randomTime--;
             console.log(randomTime);
             if(randomTime <= 0){
+                btnTime.textContent = 'CLICK !!!'
+                btnTime.style.backgroundColor = '#9BFF59'
+                let seconde = 0;
+                let miliseconde = 0;
+                const TimeTextInterval = setInterval(() => {
+                    let reactionTimeTextInt = `${seconde} : ${miliseconde}`;
+                    miliseconde++;
+                    if(miliseconde >= 100){
+                        seconde++;
+                        miliseconde = 0;
+                    }
+                    reactionTimeText.textContent = reactionTimeTextInt
+                    console.log(reactionTimeTextInt);
+                }, 10);
+                btnTime.addEventListener('click',()=>{
+                    clearInterval(TimeTextInterval)
+                })
                 clearInterval(setIntervalTime)
             }
         }, 1);
-
-        function TimeClear(){
-
-        }
 
     })
 }
